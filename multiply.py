aad1a3ba8764339e6bf037682a73ae0bc40ba263
Rocky <rockyspiker@gmail.com>
2020-01-07 10:26:16 -0700

    Added several functions to get user input Matrix

---
+++ multiply.py
@@ -0,0 +1,47 @@
+matrixOne = []
+matrixTwo = []
+
+def getMatrixOne():
+    print('Enter a matrix a row at a time. End with a blank line.')
+    getInput(1)
+
+def getMatrixTwo():
+    print('Enter a matrix. End with a blank line.')
+    getInput(2)
+
+def getInput(matrixNum):
+    isValid = True
+    inp = input(': ')
+    if (inp):
+        row = inp.split()
+        for num in row:
+            if not (num.isdigit()):
+                print(num + ' is an invalid input. Please restart this row.')
+                isValid = False
+                getInput(matrixNum)
+        if (isValid and validLen(matrixNum, len(row))):
+            if (matrixNum == 1):
+                matrixOne.append(row)
+
+def validLen(matrixNum, rowLen): # makes sure every row is the same length
+    if (matrixNum == 1):
+        if (len(matrixOne) == 0):
+            return True
+        elif (len(matrixOne[0]) == rowLen):
+            return True
+        else:
+            print('All rows must be the same length.')
+            getInput(matrixNum)
+            return False
+    elif (matrixNum == 2):
+        if (len(matrixTwo) == 0):
+            return True
+        elif (len(matrixTwo[0]) == rowLen):
+            return True
+        else:
+            print('All rows must be the same length.')
+            getInput(matrixNum)
+            return False
+    else:
+        print('System error, "MATRIX NUMBER", line 46.')
+        
