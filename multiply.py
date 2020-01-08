38fc3225801e71cab3d367a75504f13808a91f4d
Rocky <rockyspiker@gmail.com>
2020-01-07 17:35:03 -0700

    Final. Commented.

--- multiply.py
+++ multiply.py
@@ -10,17 +10,17 @@ def getMatrixTwo():
     print('Enter a matrix. End with a blank line.')
     getInput(2)
 
-def getInput(matrixNum):
+def getInput(matrixNum): # matrixNum tells the function what matrix to get the input for
     isValid = True
     inp = input(': ')
     if (inp):
         row = inp.split()
         for num in row:
-            if not (num.isdigit()):
+            if not (num.isdigit()): # makes sure that the inputs are only numbers
                 print(num + ' is an invalid input. Please restart this row.')
                 isValid = False
                 getInput(matrixNum)
-        if (isValid and validLen(matrixNum, len(row))):
+        if (isValid and validLen(matrixNum, len(row))): # makes sure it is a digit and the matrix has the same amount of col in each row
             if (matrixNum == 1):
                 matrixOne.append(row)
                 getInput(matrixNum)
@@ -28,7 +28,7 @@ def getInput(matrixNum):
                 matrixTwo.append(row)
                 getInput(matrixNum)
             else:
-                print('System error, "MATRIX NUMBER", line 30.')
+                print('System error, "MATRIX NUMBER", line 31.')
 
 def validLen(matrixNum, rowLen): # makes sure every row is the same length
     if (matrixNum == 1):
@@ -50,21 +50,21 @@ def validLen(matrixNum, rowLen): # makes sure every row is the same length
             getInput(matrixNum)
             return False
     else:
-        print('System error, "MATRIX NUMBER", line 52.')
+        print('System error, "MATRIX NUMBER", line 53.')
 
 def multiply():
     if (len(matrixOne[0]) == len(matrixTwo)):
         print('They can be multiplied.')
-        for rowOfOne in range(len(matrixOne)):
+        for rowOfOne in range(len(matrixOne)): # loop for every row in matrixOne
             row = []
-            for colOfTwo in range(len(matrixTwo[0])):
+            for colOfTwo in range(len(matrixTwo[0])): # loop for every col in matrixTwo
                 result = 0
-                for rowOfTwo in range(len(matrixTwo)):
-                    result += int(matrixOne[rowOfOne][rowOfTwo]) * int(matrixTwo[rowOfTwo][colOfTwo])
+                for rowOfTwo in range(len(matrixTwo)): # loop for every row in matrixTwo
+                    result += int(matrixOne[rowOfOne][rowOfTwo]) * int(matrixTwo[rowOfTwo][colOfTwo]) # multiplies each of matrixOne col to matrixTwo row
                 row.append(result)
             matrixResult.append(row)
-        for row in range(len(matrixResult)):
-            for col in range(len(matrixResult[0])):
+        for row in range(len(matrixResult)): # loop for every row in matrixResult
+            for col in range(len(matrixResult[0])): # loop for every col in matrixResult
                 print(matrixResult[row][col], end='\t')
             print('\n', end='')
     else:
